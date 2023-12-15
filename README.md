261e1053f02619403c463425617adce330e8769a
Delistoianov <joao.gonzalez@sou.inteli.edu.br>
2023-12-15 15:15:13 -0300

    Prova B M2

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# 20232BSET03P2
+# Documentação atividade prova
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+No readme, descreva as vulnerabilidades identificadas e as medidas adotadas para corrigir cada uma delas
+
+Vulnerabilidades identificadas: 
+
+1. Funcionamento incorreto da lógica.
+2. Operados "${}" em requisições POST são portas de entrada para SQL injection.
+
+
+Medidas adotas:
+
+1. A lógica do código está condizente com a estrutura antiga, no entanto, agora está em funcionemento. A lógica está descrita abaixo:
+
+    1. Usuário faz um post no body, que espera receber a chave "name" e o valor "cats" ou "dogs" a depender do endpoint em questão.
+        * Como resposta se espera:
+        `            {
+  "id": 1,
+  "name": "dogs",
+  "votes": 0
+} ou {
+  "id": 1,
+  "name": "cats",
+  "votes": 0
+}
+`
+    2. Após isso pode-se fazer um get, para testar a funcionalidade do votar.
+        * Como resposta se espera:
+    `  {
+    "id": 1,
+    "name": "cats",
+    "votes": 0
+  } ou {
+    "id": 1,
+    "name": "dogs",
+    "votes": 0
+  } `
+
+    3. Usuário vota em algum dos dois bixos:
+        * Como resposta espera-se:
+        `""Voto computado"`
+
+    4. Para mostrar que o funcionamento está correto, agora pode-se fazer um get novamente.
+        * Como resposta espera-se:
+    ` {
+    "id": 1,
+    "name": "dogs",
+    "votes": 1
+  } ou     "id": 1,
+    "name": "cats",
+    "votes": 1`
+
+    Observe que agora os dados foram computados.
+
+2. Melhora na segurança:
+
+* Para evitar ataques de SQL injection foi retirado os operadores "${}" dos endpoints POST que continham body. Para previnir esse tipo de problema a tag "${}" foi substituida por "(?, 0)", que é um metodo mais seguro
+
+* Além disso, utilizei a biblioteca express-validator para validar parâmetros das rotas,   
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const { param, check, validationResult } = require('express-validator');
 
 const app = express();
 const port = 3000;
@@ -11,30 +12,95 @@ app.use(bodyParser.json());
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
 });
 
-app.post('/cats', (req, res) => {
-  const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
-    if (err) {
-      res.status(500).send("Erro ao inserir no banco de dados");
-    } else {
-      res.status(201).json({ id: this.lastID, name, votes: 0 });
+app.post('/cats',
+  check('name').isLength({ min: 1, max: 20 }).withMessage('Nome é obrigatório'),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  });
-});
 
-app.post('/dogs', (req, res) => {
-  
-});
+    const name = req.body.name;
 
-app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
-});
+    if (name != "cats") {
+      res.status(400).send("Nome é invalido");
+    }
+
+    else {
+      db.run(`INSERT INTO cats (name, votes) VALUES (?, 0)`, [name], function (err) {
+        if (err) {
+          res.status(500).send("Erro ao inserir no banco de dados");
+        } else {
+          res.status(201).json({ id: this.lastID, name, votes: 0 });
+        }
+      });
+    }
+  }
+);
+
+app.post('/dogs',
+  check('name').isLength({ min: 1, max: 20 }).withMessage('Nome é obrigatório'),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const name = req.body.name;
+
+    if (name != "dogs") {
+      res.status(400).send("Nome invalido");
+    }
+
+    else {
+      db.run(`INSERT INTO dogs (name, votes) VALUES (?, 0)`, [name], function (err) {
+        if (err) {
+          res.status(500).send("Erro ao inserir no banco de dados");
+        } else {
+          res.status(201).json({ id: this.lastID, name, votes: 0 });
+        }
+      });
+    }
+
+  }
+);
+
+app.post('/vote/:animalType/:id',
+  param('animalType').isIn(['dogs', 'cats']).withMessage('Animal inválido'),
+  param('id').isNumeric().withMessage('ID inválido'),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const animalType = req.params.animalType;
+    const id = req.params.id;
+
+    //verifiqca se o registro do animal existe antes de adicionar um voto
+
+    db.get(`SELECT id FROM ${animalType} WHERE id = ?`, [id], (err, row) => {
+      if (err) {
+        res.status(500).send("Erro ao consultar o banco de dados");
+      } else if (!row) {
+        res.status(404).send("Animal não encontrado");
+      } else {
+        db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ?`, [id], function (err) {
+          if (err) {
+            res.status(500).send("Erro ao atualizar o voto no banco de dados");
+          } else {
+            res.status(200).send("Voto computado");
+          }
+        });
+      }
+    });
+  }
+);
+
 
 app.get('/cats', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
@@ -47,7 +113,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
